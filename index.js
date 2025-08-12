7d71abe624fd73785915b41cebe74f635cce9ed8
Bugg4 <bulgarelli.marco99@gmail.com>
2025-08-13 01:05:21 +0200

    wip

--- index.js
+++ index.js
@@ -1,44 +1,87 @@
 // rules/no-gif-allowed.js
+import { match } from "node:assert";
+import { text } from "node:stream/consumers";
 import { lintRule } from "unified-lint-rule";
 import { visit } from "unist-util-visit";
 
-function checkWord(node, options){
-    const words = options.words;
+function fixNode(node, file, options) {
+  // correct casings
+  const user_word_list = options.words;
 
-    console.log(words);
+  //console.log(user_word_list);
 
-      const regex = new RegExp(`\\b(${words.join("|")})\\b`, "g");
-      const matches = node.value.match(regex);
+  // create pattern to match all words in the list with any casing
+  // regex to match a list words, case insenstive, allow adjacent punctuation before and after:
+  const pattern = new RegExp(`\\b(${user_word_list.join("|")})\\b`, "gi");
 
-      if (matches) {
-        matches.forEach((word) => {
-          if (word !== word.toLowerCase()) {
-            file.message(`Incorrect casing for word: "${word}"`, node);
-          }
-        });
+  const matches = node.value.match(pattern);
+
+  if (matches) {
+    matches.forEach((match) => {
+      node.actual = match;
+      node.expected = user_word_list.find(
+        (word) => word.toLowerCase() === match.toLowerCase(),
+      );
+      node.file = file;
+      
+      file.message("Wrong word case", node);
+    });
+  }
+
+  //let results = [];
+
+  /*   if (matches) {
+    matches.forEach((match, i) => {
+      const result = {
+        found: match,
+        at: node.position,
+        expected: user_word_list.find(
+          (word) => word.toLowerCase() === match.toLowerCase(),
+        ),
+      };
+
+      if (result.found !== result.expected) {
+        results.push(result);
       }
+    });
+  }
+
+  return results; */
 }
 
-const remarkLintWordCase = lintRule(
-  "remark-lint-word-case: ",
-  function (tree, file, options) {
-    if (!options) {
-      file.message("The `words` option is required.", options);
-      return;
-    }
-
-    if (!Array.isArray(options.words) || options.words.length === 0) {
-      file.message("The `words` option must be a non-empty array", options);
-      return;
-    }
-
-    if (options.words.some((word) => typeof word !== "string")) {
-      file.message("All items in the `words` array must be strings", options);
-      return;
-    }
-
-    visit(tree, "text", checkWord);
+function wordCaseRule(tree, file, options = {}) {
+  // check options are valid
+  if (!options) {
+    file.message("The `words` option is required.", options);
+    return;
   }
-);
+
+  if (!Array.isArray(options.words) || options.words.length === 0) {
+    file.message("The `words` option must be a non-empty array", options);
+    return;
+  }
+
+  if (options.words.some((word) => typeof word !== "string")) {
+    file.message("All items in the `words` array must be strings", options);
+    return;
+  }
+
+  // https://github.com/syntax-tree/mdast?tab=readme-ov-file#nodes
+  // blockquote break code definition emphasis heading html image imageReference inlineCode link linkReference list listItem paragraph root strong text thematicBreak
+  visit(tree, "text", (node) => {
+    // console.log(node);
+    const corrections = fixNode(node, file, options);
+
+    if (!corrections || corrections.length === 0) {
+      return;
+    }
+
+    corrections.forEach((corr) => {
+      file.message("Incorrect word case", node);
+    });
+  });
+}
+
+const remarkLintWordCase = lintRule("remark-lint:word-case", wordCaseRule);
 
 export default remarkLintWordCase;
---
+++ tests/invalid.md
@@ -0,0 +1,28 @@
+# Here's an example of an invalid document
+
+## regex has the wrong casing
+
+- We're expecting `RegEx` but I wrote `regex` instead
+- words in code blocks or fenced code blocks should not be corrected
+
+Let's see if we can correct table cells:
+
+| Good  | Not Good |
+|-------|----------|
+| RegEx | regex    |
+| Foo   | bar      |
+| bar   | Foo      |
+| foo   | Foo      |
+
+- Blockquote:
+
+> first line
+> foobar
+> FOOBAR
+> regex here
+
+1. Does it work if I put adjacent punctuation? Like regex, regex? 
+2. testing fooBAR
+3. testing FooBAR
+4. testing foo BAR
+5. testing barFOO
