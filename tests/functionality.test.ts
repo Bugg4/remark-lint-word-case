9bf9e0353b066b1ef091cf876c8465734ca33e8c
Bugg4 <bulgarelli.marco99@gmail.com>
2025-08-18 22:07:39 +0200

    tests: add Blockquote
    Also check that each word we match (and therefore run the plugin on) is actually correct and corresponds to a word in the user provided word list

--- tests/functionality.test.ts
+++ tests/functionality.test.ts
@@ -4,24 +4,39 @@ import { remark } from "remark";
 import type { VFile } from "vfile";
 import { reporter } from "vfile-reporter";
 import remarkLintWordCase from "../src/index.ts";
+import { NonEmptyArray } from "../src/utils.ts";
 
 const invalidFilePath = path.join(import.meta.dirname, "docs/invalid.md");
 const invalidFileContent = await Bun.file(invalidFilePath).text();
 
 test("Complete file", async () => {
+  const WORDS: NonEmptyArray<string> = ["RegEx", "fooBAR"];
+
   const result: VFile = await remark()
     .use(remarkLintWordCase, {
-      words: ["RegEx", "fooBAR"],
+      words: WORDS,
     })
     .process(invalidFileContent);
 
   result.path = invalidFilePath;
 
-  if (Bun.env.LOGGING) {
+  if (Bun.env.DEBUG) {
     console.log(reporter(result));
   }
 
+  // asser correct warnign quantity
   expect(result.messages.length).toBe(13);
+
+  const WORDS_LC = WORDS.map((w) => w.toLocaleLowerCase());
+
+  // Assert that we're only matching words that are inside the wordlist
+  result.messages.forEach((m) => {
+    expect(m.actual);
+
+    if (m.actual) {
+      expect(WORDS_LC.includes(m.actual.toLocaleLowerCase())).toBe(true);
+    }
+  });
 });
 
 test("Heading", async () => {
@@ -31,8 +46,34 @@ test("Heading", async () => {
     })
     .process("# this is a foobar test");
 
-  if (Bun.env.LOGGING) {
+  if (Bun.env.DEBUG) {
     console.log(reporter(result));
   }
+  //correct quantity
   expect(result.messages.length).toBe(1);
+
+  expect(result.messages[0].actual).toBe("foobar");
+  expect(result.messages[0].expected?.at(0)).toBe("fooBAR");
+});
+
+test("Block Quote", async () => {
+  const result = await remark().use(remarkLintWordCase, {
+    words: ["fooBAR"],
+  }).process(`> this is a foobar blockquote  
+on multiple lines, foobar!
+>
+>
+> foobar
+      `);
+
+  if (Bun.env.DEBUG) {
+    console.log(reporter(result));
+  }
+  //correct quantity
+  expect(result.messages.length).toBe(3);
+
+  result.messages.forEach((m) => {
+    expect(m.actual).toBe("foobar");
+    expect(m.expected?.at(0)).toBe("fooBAR");
+  });
 });
